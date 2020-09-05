29d26d0a0b802a677342e8838470c7a8159922c6
UlisesSG <ukisessg@gmail.com>
2020-09-04 20:14:17 -0500

    Resultado de busqueda refactorizado

--- src/components/hotel/ResultadoBusqueda.jsx
+++ src/components/hotel/ResultadoBusqueda.jsx
@@ -1,51 +1,49 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import '../../styles/resultadoBusqueda.css';
 
 import hotel from '../../img/hotel.jpg';
 
-class ResultadoBusqueda extends PureComponent {
-  render() {
-    return (
-      <div className='resultado'>
-        <img className='resultado__img' src={hotel} alt='Imagen de hotel' />
-        <br />
-        <table className='info'>
-          <thead>
-            <tr>
-              <th>
-                <h2>Nombre de hotel</h2>
-              </th>
-            </tr>
-          </thead>
-          <tbody className='detalles'>
-            <tr>
-              <td>
-                <p>
-                  <strong>Precio: </strong>
-                  <strong>{Math.floor(Math.random() * 1200)}</strong>
-                </p>
-              </td>
-              <td>
-                <p>
-                  <strong>Huespedes: </strong>
-                  <strong>
-                    {Math.floor(Math.random() * (2000 - 500) + 500)}
-                  </strong>
-                </p>
-              </td>
-              <td>
-                <p>
-                  <strong>Ubicación: </strong>
-                  <strong>Colombia</strong>
-                </p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+const ResultadoBusqueda = () => {
+  return (
+    <div className='resultado'>
+      <img className='resultado__img' src={hotel} alt='Imagen de hotel' />
+      <br />
+      <table className='info'>
+        <thead>
+          <tr>
+            <th>
+              <h2>Nombre de hotel</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody className='detalles'>
+          <tr>
+            <td>
+              <p>
+                <strong>Precio: </strong>
+                <strong>{Math.floor(Math.random() * 1200)}</strong>
+              </p>
+            </td>
+            <td>
+              <p>
+                <strong>Huespedes: </strong>
+                <strong>
+                  {Math.floor(Math.random() * (2000 - 500) + 500)}
+                </strong>
+              </p>
+            </td>
+            <td>
+              <p>
+                <strong>Ubicación: </strong>
+                <strong>Colombia</strong>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ResultadoBusqueda;
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js',
-    publicPath: './',
+    publicPath: '/',
   },
 
   resolve: {
